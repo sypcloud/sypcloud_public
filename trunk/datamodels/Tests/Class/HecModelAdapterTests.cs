aecae68ad1992d0ed24d2c94d7c5cdeced7895dc
jerzypeng <jerzy.peng@gmail.com>
2022-04-16 19:18:40 +0800

    add test project

---
+++ trunk/datamodels/Tests/Class/HecModelAdapterTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SY.HECModelAdapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SY.HECModelAdapter.Tests
+{
+    [TestClass()]
+    public class HecModelAdapterTests
+    {
+        [TestMethod()]
+        public void RunModelTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
