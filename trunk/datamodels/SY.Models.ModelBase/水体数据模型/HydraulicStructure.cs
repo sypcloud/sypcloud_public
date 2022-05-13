32df2161cf712e6079cc2dd034a3a41fc439c8ad
sypcloud <80941918@qq.com>
2022-05-13 22:30:33 +0800

    update

--- trunk/datamodels/SY.Models.ModelBase/水体数据模型/HydraulicStructure.cs
+++ trunk/datamodels/SY.Models.ModelBase/水体数据模型/HydraulicStructure.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sy.Global;
 
 namespace SY.Models.ModelBase
 {
@@ -10,11 +11,13 @@ namespace SY.Models.ModelBase
     {
         public string RiverName { get; set; }
         public float Chainage { get; set; }
+        public List<PointD> Location { get; set; }
         public string ID { get; set; }
         public string TopID { get; set; }
         public List<float> Level { get; set; }
         public List<float> Width { get; set; }
         public int Type { get; set; }
         public int ControlStrategyType { get; set; }
+        public string coordinate_type { get; set; }
     }
 }
